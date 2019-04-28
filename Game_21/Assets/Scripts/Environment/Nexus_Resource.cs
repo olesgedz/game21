28333bf20f768267b85095f8027983d5e8c23866
Shantel Bosmer <sbosmer@oa-l3.21-school.ru>
2019-04-28 23:23:50 +0300

    pathfinding

---
+++ Game_21/Assets/Scripts/Environment/Nexus_Resource.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nexus_Resource : MonoBehaviour
+{
+    public int resource1;
+	public int resource2;
+}
