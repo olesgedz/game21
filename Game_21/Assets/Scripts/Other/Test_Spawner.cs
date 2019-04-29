cfed65160a7db83f2c3ec87caaa18761a83f1d29
Shantel Bosmer <sbosmer@oa-l3.21-school.ru>
2019-04-29 09:01:59 +0300

    imma firin mah laisah

--- Game_21/Assets/Scripts/Other/Test_Spawner.cs
+++ Game_21/Assets/Scripts/Other/Test_Spawner.cs
@@ -8,11 +8,32 @@ public class Test_Spawner : MonoBehaviour
 	public GameObject enemyPrefab;
 	public float frequency;
 	private float timer;
+	private bool abc = true;
+	private Grid gr;
+	private List<GameObject> family;
+
+	void Start() {
+		gr = GetComponent<Grid>();
+		family = new List<GameObject>();
+	}
 
 	void Update() {
-		if (Time.time > timer + frequency) {
-			Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+		if (Time.time > timer + frequency && abc) {
+			abc = false;
+			GameObject temp = Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+			family.Add(temp);
 			timer = Time.time;
+		}
+	}
+
+	public void removeMe(GameObject woof) {
+		family.Remove(woof);
+	}
+
+	public void buildingBuilt() {
+		gr.CreateGrid();
+		foreach (GameObject broda in family) {
+			broda.GetComponent<Unit>().ReCalcPath(); // REVIEW possible bottleneck
 		}
 	}
 }
